eb0814aa835ef5136748abfe7c7e80d23af32b97
Черепахин Василий <vasi.che@gmail.com>
2024-11-27 16:07:29 +0500

    add MessageControllerTest.getMessageByIdNotExist()

--- consumer/src/test/java/ru/perm/v/camel/simple_kafka/consumer/controller/MessageControllerTest.java
+++ consumer/src/test/java/ru/perm/v/camel/simple_kafka/consumer/controller/MessageControllerTest.java
@@ -2,6 +2,7 @@ package ru.perm.v.camel.simple_kafka.consumer.controller;
 
 
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import ru.perm.v.camel.simple_kafka.consumer.dto.MessageDTO;
 import ru.perm.v.camel.simple_kafka.consumer.repository.MessageEntity;
@@ -13,6 +14,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -34,6 +36,7 @@ class MessageControllerTest {
         assertEquals(new MessageDTO(UUID.fromString(validUUID1), "NAME1", "DESCR1"), messages.get(0));
         assertEquals(new MessageDTO(UUID.fromString(validUUID2), "NAME2", "DESCR2"), messages.get(1));
     }
+
     @Test
     void getAllMessagesForEmpty() {
         MessageRepository messageRepository = mock(MessageRepository.class);
@@ -48,7 +51,7 @@ class MessageControllerTest {
     @Test
     void getMessageByIdExist() {
         MessageRepository messageRepository = mock(MessageRepository.class);
-        UUID uuid = new UUID(2,2);
+        UUID uuid = new UUID(2, 2);
         MessageEntity messageEntity = new MessageEntity(uuid, "NAME", "DESCRIPTION");
         when(messageRepository.findById(uuid)).thenReturn(Optional.of(messageEntity));
         MessageController ctrl = new MessageController(messageRepository);
@@ -58,5 +61,18 @@ class MessageControllerTest {
         assertEquals(uuid, Objects.requireNonNull(receivedMessage.getBody()).getId());
         assertEquals("NAME", receivedMessage.getBody().getName());
         assertEquals("DESCRIPTION", receivedMessage.getBody().getDescription());
+    }
+
+    @Test
+    void getMessageByIdNotExist() {
+        UUID uuid = new UUID(2, 2);
+        MessageRepository messageRepository = mock(MessageRepository.class);
+        when(messageRepository.findById(uuid)).thenReturn(Optional.empty());
+        MessageController ctrl = new MessageController(messageRepository);
+
+        ResponseEntity<MessageDTO> receivedMessage = ctrl.getMessageById(uuid);
+
+        assertNull(receivedMessage.getBody());
+        assertEquals(HttpStatus.NOT_FOUND, receivedMessage.getStatusCode());
     }
 }
