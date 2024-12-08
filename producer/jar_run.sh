ac13364d90aa1c4bfa75621d55e05568a8be2235
Черепахин Василий <vasi.che@gmail.com>
2024-12-08 17:20:37 +0500

    add jar_run.sh

---
+++ producer/jar_run.sh
@@ -0,0 +1,1 @@
+/usr/lib/jvm/openjdk-17/bin/java -jar target/producer-0.0.1-SNAPSHOT.jar
--- producer/run.sh
+++ producer/run.sh
@@ -1,1 +1,2 @@
+/usr/lib/jvm/openjdk-17/bin/java -jar producer-0.0.1-SNAPSHOT.jar
 ./mvnw clean spring-boot:run
