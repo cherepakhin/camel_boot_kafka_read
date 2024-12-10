44a007d9a2f39d0bc764368176ae015c95da984b
Черепахин Василий <vasi.che@gmail.com>
2024-12-10 20:48:31 +0500

    fix MessageEntityTest

--- consumer/src/test/java/ru/perm/v/camel/simple_kafka/consumer/repository/MessageEntityTest.java
+++ consumer/src/test/java/ru/perm/v/camel/simple_kafka/consumer/repository/MessageEntityTest.java
@@ -16,4 +16,12 @@ class MessageEntityTest {
         assertEquals("NAME2", messageEntity.getName());
         assertEquals("DESCRIPTION2", messageEntity.getDescription());
     }
+
+    @Test
+    void equalsTest() {
+        MessageEntity messageEntity1 = new MessageEntity("9501f96f-29ba-40e4-9b08-d3db418d96dc", "NAME2", "DESCRIPTION2");
+        MessageEntity messageEntity2 = new MessageEntity("9501f96f-29ba-40e4-9b08-d3db418d96dc", "NAME2", "DESCRIPTION2");
+
+        assertEquals(messageEntity1, messageEntity2);
+    }
 }
